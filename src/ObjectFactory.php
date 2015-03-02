10d88ad1d47e6cc34f47ba2cb23599d3153c300e
cristian.deveng@gmail.com <cristian.deveng@gmail.com>
2015-03-02 16:30:34 -0300

    First Commit

---
+++ src/ObjectFactory.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace UtilityCommon;
+
+trait ObjectFactory
+{
+	public function __set($atrib, $value)
+    {
+    	if (in_array($atrib, $this->getKeys()))
+        	$this->$atrib = $value;
+    }
+ 
+    public function __get($atrib)
+    {
+     
+        return $this->$atrib;
+    }
+
+    public function __isset($name)
+    {
+        return in_array($name, $this->getKeys());
+    }
+
+    public function getKeys()
+    {
+    	return array_keys($this->toArray());
+    }
+
+    public function toArray()
+    {
+    	
+    	return $this->_toArray();
+    }
+
+    private function _toArray()
+    {
+    	$vars = get_object_vars($this);
+
+    	foreach ($vars as $key => $value) 
+    	{
+    		if (is_object($value))
+    		{
+    			$vars[$key] = method_exists($value, 'toArray') ? $value->toArray() : (array)$value;
+    		} else
+    		if (is_array($value))
+    			$vars[$key] = $value;
+    	}
+
+    	return $vars;
+    }
+}
